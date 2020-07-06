c290a26f74596f4c71446fc3ab919543fb66f418
Rami <info@rnweb.dk>
2020-07-07 01:18:59 +0200

    Contact Update & Delete Routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -65,15 +65,72 @@ router.post(
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+  // res.send('Update contact');
+  const { name, email, phone, type } = req.body;
+
+  // Build cibtact object
+  const contactFields = {};
+
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    // make sure contact is available
+    if (!contact) {
+      return res.status(400).json({ msg: 'Contact not found' });
+    }
+
+    // make sure user owns contact before update
+    if (contact.user.toString() !== req.user.id) {
+      // didn't match
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+  // res.send('Delete contact');
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    // make sure contact is available
+    if (!contact) {
+      return res.status(400).json({ msg: 'Contact not found' });
+    }
+
+    // make sure user owns contact before update
+    if (contact.user.toString() !== req.user.id) {
+      // didn't match
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Contact Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
